90e6b1a280fe658fdd51fa5d5317ad2a1f49b19b
srshanm <sridhar.shanmugam@unfcu.com>
2021-02-02 16:14:32 -0500

    Added 2nd Class

---
+++ src/test/MySecondClass.java
@@ -0,0 +1,10 @@
+package test;
+
+public class MySecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my 2nd Class");
+	}
+
+}
